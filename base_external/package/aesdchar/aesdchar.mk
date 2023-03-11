e13bbbfe9a7e9cbca8eb450a7e34e8d0aace3af6
HJ <cafe@computer.org>
2023-03-10 23:39:08 -0600

    assignment-8-complete

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,33 +1,22 @@
 
 ##############################################################
 #
-# LDD
+# AESD CHAR
 #
 ##############################################################
 
-LDD_VERSION = 95e83ffdb135ab313b7667df797a771249f25a4e
+#AESDCHAR_VERSION = 3af8792b9f64d285be19f2c676a08d9f8dffad8c
+
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 
-LDD_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-neocafe88.git
-LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
+AESDCHAR_SITE = ../../assignments-3-and-later-neocafe88
+AESDCHAR_SITE_METHOD = local
+#AESDCHAR_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = aesd-char-driver
-
-
-#define KERNEL_MODULE_BUILD_CMDS
-#	$(MAKE) -C '$(@D)' LINUX_DIR='$(LINUX_DIR)' CC='$(TARGET_CC)' LD='$(TARGET_LD)' modules
-#endef
-
-#define LDD_INSTALL_TARGET_CMDS
-#	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/sbin
-#	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/sbin
-#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/sbin
-#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/sbin
-#endef
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
